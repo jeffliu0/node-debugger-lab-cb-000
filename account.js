098be182d03659640390d9dad978e943b048ae0b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-24 19:05:34 +0000

    automatically backed up by learn

--- account.js
+++ account.js
@@ -23,7 +23,7 @@ module.exports = function account() {
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
   openAccount(customerAccount, deposit)
   setAccountType()
